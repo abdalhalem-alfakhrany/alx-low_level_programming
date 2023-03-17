d9d64165656650ae5277c777156f56df2ff987e4
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-17 12:14:32 -0700

    descripe number positive or negative Task modified (betty styeling)

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,17 +5,17 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-  int n;
+	int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-  printf("%d ", n);
-  if (n > 0)
-    printf("is positive\n");
-  else if (n == 0)
-    printf("is zero\n");
-  else
-    printf("is negative\n");
-  return (0);
+	printf("%d ", n);
+	if (n > 0)
+		printf("is positive\n");
+	else if (n == 0)
+		printf("is zero\n");
+	else
+		printf("is negative\n");
+	return (0);
 }
