2e2969ef877d0a82ed667b40caf9347a63759b23
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-17 15:42:57 -0700

    print more stanger combination Task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,11 @@ int main(void)
 		for (j = i + 1; j <= 99; j++)
 		{
 
-			putchar((i % 10) + 48);
 			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
 			putchar(' ');
-			putchar((j % 10) + 48);
 			putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
 			if (i == 98 && j == 99)
 				break;
 			putchar(',');
