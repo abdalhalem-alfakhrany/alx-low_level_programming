03b0222c4548d78b8ebdd419e5d25dafd80e4ca1
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-21 14:31:34 -0700

    chack the number is positev or nigative Task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - descripte the number positive or nigative
+ * @n: the number to be checked
+ * Return: 0 if number is 0, 1 if number is positive, -1 if number is nigative
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
