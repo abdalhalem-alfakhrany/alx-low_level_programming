48151d8c8ac55be69da70af72c596b419989c3dc
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-20 22:38:18 -0700

    print alphabet Task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <_putchar.c>
+
+void print_alphbet(void)
+{
+	char i;
+
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+/**
+* main - check the code
+* Return: Always 0
+*/
+int main(void)
+{
+	return (0);
+}
