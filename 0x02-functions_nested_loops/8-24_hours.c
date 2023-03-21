2886268913cf23b4b09f9555a531222b666d24e2
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-21 16:02:54 -0700

    jack baure logest day Task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,14 @@
 #include "main.h"
 #include "stdio.h"
+
+/**
+  * jack_bauer - print every minuite of jack bauer longest
+  * Return: void
+ */
 void jack_bauer(void)
 {
 	int h, m;
+
 	for (h = 0; h < 24; h++)
 		for (m = 0; m < 60; m++)
 		{
