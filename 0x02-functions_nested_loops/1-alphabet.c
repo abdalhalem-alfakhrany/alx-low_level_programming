dbccabd46d6a04d403d8c94627369aed8739a068
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-21 05:21:46 -0700

    print alphabet Task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,14 @@
 #include "main.h"
 
+/**
+  * print_alphabet - print all alphabet leeters to stdout
+  * Return: void not returning anything
+  */
 void print_alphabet(void)
 {
 	char i;
 
-	for(i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
