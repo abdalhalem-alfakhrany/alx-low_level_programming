367ec4ca47e5aeb233dcbfd97408d7f1e86741f3
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-21 16:31:29 -0700

    adding tow numbers throught function add Task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* add - adding tow functions
+* @a: first number
+* @b: second number
+* Return: the sum of thes tow numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
