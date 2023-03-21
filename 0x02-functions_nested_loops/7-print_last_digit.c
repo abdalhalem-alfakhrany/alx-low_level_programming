1d44aff83eaa956b4ef1871d9bc7d0be2835e515
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-21 15:15:11 -0700

    print the last digit of number Task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_last_digit - printing the last digit of numerb to stdout
+* @n: the number to print tha last digit of
+* Return: the last digit of number n
+ */
+int print_last_digit(int n)
+{
+	int lastDigit = ((n < 0) ? n * -1 : n) % 10;
+
+	_putchar(lastDigit + '0');
+	return (lastDigit);
+}
