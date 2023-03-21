085ac7af9b6e52900f3ee0534da9a4e79db9548f
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-21 04:54:21 -0700

    print _putchar Task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,12 +5,13 @@
 */
 int main(void)
 {
-	char * str = "_putchar\n";
+	char * str = "_putchar\0";
 	int i;
 	
-	for (i = 0; i != '\n'; i++)
+	for (i = 0; i <= 8; i++)
 	{
 		_putchar(str[i]);
 	}
+	_putchar('\n');
 	return (0);
 }
