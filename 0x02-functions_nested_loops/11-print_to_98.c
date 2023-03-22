6fecd26a0d64d6673e70877ef39c943ada471842
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-21 17:44:58 -0700

    print all numerbs to 89 Task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include "stdio.h"
+/**
+* print_to_98 - print all numbers to 98
+* @n: the number to start from
+* Return: void
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+		for (i = n; i <= 98; i++)
+		{
+			if (n == 98)
+			{
+				printf("%2d", i);
+				continue;
+			}
+			printf("%2d, ", i);
+		}
+	else
+		for (i = n; i >= 98; i--)
+		{
+			if (n == 98)
+			{
+				printf("%2d", i);
+				continue;
+			}
+			printf("%2d, ", i);
+		}
+	printf("\n");
+}
