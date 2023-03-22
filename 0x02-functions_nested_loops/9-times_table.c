f0f6a536bb5f9e8fc6cd3b8bb39201739f030b94
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-21 17:59:45 -0700

    print times table Task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,12 +13,12 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (j == 9)
+			if (j == 0)
 			{
 				printf("%2d", i * j);
 				continue;
 			}
-			printf("%2d, ", i * j);
+			printf(", %2d", i * j);
 		}
 		printf("\n");
 	}
