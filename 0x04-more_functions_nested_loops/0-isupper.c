b5fee9d028b7969f37be5a16d99844789ae65200
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-23 05:08:22 -0700

    check is char upper case or lower case Task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+* _isupper - check is char c upper case or lower case
+* @c: char to be checked
+* Return: 1 if char c is upper 0 other wise
+ */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
