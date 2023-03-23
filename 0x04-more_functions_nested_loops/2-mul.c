05079e89d1273f99c61a7724971d96f920dfd906
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-23 05:18:33 -0700

    multibly two numbers Task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - useless function to multibly two numbers
+ * @a: first number
+ * @b: second number
+ * Return: multiblication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
