6ad341fce3c074cacdfbd3a3ca909ba95c076f13
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-23 16:11:08 -0700

    remaining days in year Task

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0) || (year % 400 == 0) || (year % 100 == 0))
+	if ((!(year % 4) && !(year % 400)) || (year % 100))
 	{
 		if (month >= 2 && day >= 60)
 			day++;
@@ -22,7 +22,6 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		printf("not leep year\n");
 		if (month == 2 && day == 60)
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		else
