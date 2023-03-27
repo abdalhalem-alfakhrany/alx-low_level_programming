fec535685fa582c684c9b08c404191c35454f7a0
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-27 14:37:24 -0700

    swap numebers values Task

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * swap_int - swap int a, b values
+ * @a: refrance to a variable
+ * @b: refrance to b variable
+ * Return: void
+ */
 void swap_int(int *a, int *b)
 {
 	int tmp = *a;
