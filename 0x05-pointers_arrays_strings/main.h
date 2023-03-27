130e49aad168036e34f3e86892a8f90ae05697d6
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-27 16:55:38 -0700

    print string to stdout Task

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+void _putchar(char);
+
 void reset_to_98(int *);
 void swap_int(int *, int *);
 int _strlen(char *);
+void _puts(char *);
 void rev_string(char *);
 
 #endif
