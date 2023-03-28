db0c838e7d31424449299bf9c7233fe87df8cb88
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-27 20:37:31 -0700

    convert string to number Task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,4 @@
-#include "math.h"
+#include "ctype.h"
 #include "stdio.h"
 #include "string.h"
 #include "main.h"
@@ -10,37 +10,24 @@
 */
 int _atoi(char *s)
 {
-	int length = strlen(s),	i, lastSign = 0;
-	int	numberStrLast = 0, startNumber, number;
-	char numberStr[32];
-
-	for (i = 0; i < 32; i++)
-		numberStr[i] = '\0';
+	int length = strlen(s),	i, j, lastSign = 0, number = 0;
+	
 	for (i = 0; i < length; i++)
 	{
-		char current = s[i];
-
-		if (current == '+')
+		if (isdigit(s[i]))
 		{
-			lastSign += 1;
-			continue;
-		}
-		if (current == '-')
-		{
-			lastSign -= 1;
-			continue;
-		}
-		if (current >= '0' && current <= '9')
-		{
-			startNumber = 1;
-			numberStr[numberStrLast++] = current;
-			continue;
-		}
-
-		if (startNumber)
+			for (j = i; isdigit(s[j]); ++j)
+				;
+			j-=1;
+			for (; j >= i ; j--)
+				number = (number * 10) + (s[j] - '0');
 			break;
+		}
+	
+		if (s[i] == '+')
+			lastSign += 1;
+		if (s[i] == '-')
+			lastSign -= 1;
 	}
-	for (i = 0; i < numberStrLast; i++)
-		number = number * 10 + (numberStr[i] - '0');
 	return ((lastSign >= 0) ? number : -1 * number);
 }
