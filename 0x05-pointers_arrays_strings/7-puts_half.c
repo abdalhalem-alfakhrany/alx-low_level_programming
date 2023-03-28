4d1334966bbf98e34c9545374a48bbc54e095ace
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-27 17:26:51 -0700

    print half string Task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * puts_half - print the half of string
+ * @str: the string to be printed
+ * Return: void
+*/
+void puts_half(char *str)
+{
+	int length = strlen(str), i;
+
+	for (i = length / 2; i <= length; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
