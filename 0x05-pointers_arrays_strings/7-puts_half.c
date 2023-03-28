d1981d85d55942d5333fd0d4aed2faee736aac64
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-27 17:42:17 -0700

    print half string Task

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,8 +10,7 @@ void puts_half(char *str)
 {
 	int length = strlen(str), i;
 
-	/* for (i = (length % 2 == 0) ? length / 2 : (length - 1) / 2; i < length; i++) */
-	for (i = length / 2; i < length; i++)
+	for (i = (length % 2 == 0) ? length / 2 : (length + 1) / 2; i < length; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
