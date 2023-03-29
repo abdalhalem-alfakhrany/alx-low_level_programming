2b04ecba0d168bf3bcce1ed7a30d4f0bea20a2b6
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-29 13:28:10 -0700

    concatenate strings Task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,11 @@ char *_strcat(char *dest, char *src)
 
 	for (i = 0; dest[i] != '\0'; i++)
 		;
-
+	
 	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i++] = src[j];
 	}
-	dest[++i] = '\0';
+	dest[i + 1] = '\0';
 	return (dest);
 }
