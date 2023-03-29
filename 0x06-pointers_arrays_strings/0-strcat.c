501bc5c3888d739beb9fb290b21d78fcff2f5945
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-29 08:10:16 -0700

    concatenate strings Task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - concatinate tow strings
+ * @dest: the string to be appended to
+ * @src: the string to be appended
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		;
+	}
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i++] = src[j];
+	}
+	return (dest);
+}
