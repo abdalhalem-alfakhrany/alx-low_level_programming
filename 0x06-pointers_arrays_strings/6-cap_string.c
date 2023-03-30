ca0c9a87afb746099b756ccbfdc98abba8c935cf
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-30 16:07:19 -0700

    captlize the string Task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,25 +4,6 @@
 #include "ctype.h"
 
 /**
- * issperator - check if char is in sperators list
- * @c: the char to check
- * Return: 0 if not in list 1 if in list 2 if quatl to \t
- */
-int issperator(char c)
-{
-	int i;
-	char sperators[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', ' '};
-
-	if (c == '\t')
-		return (2);
-
-	for (i = 0; sperators[i] != '\0'; i++)
-		if (c == sperators[i])
-			return (1);
-	return (0);
-}
-
-/**
  * cap_string - captlize the words
  * @str: the string to bec captalized
  * Return: pointer to string
