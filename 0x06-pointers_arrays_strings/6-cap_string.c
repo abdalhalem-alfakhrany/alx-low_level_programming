0bb47d0394098c6378aac3e59ea86998db986df1
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-30 16:10:58 -0700

    captlize the string Task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *str)
 {
 	int i, strLength = strlen(str);
 	char sperators[] = {',', ';', '.', '!', '?',
-	   '"', '(', ')', '{', '}', '\n', ' ', '\t'};
+	   '\"', '(', ')', '{', '}', '\n', ' ', '\t'};
 
 	for (i = 0; i < strLength; i++)
 	{
