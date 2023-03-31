65ef18d3b969412a3d4439657d61f0949f9ce479
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-03-30 17:22:32 -0700

    the leet speach Task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,42 +9,29 @@
 char *leet(char *str)
 {
 	int i, strLength = strlen(str);
-
+	char leetTable[8][2] = {
+		"oO",
+		"lL",
+		"",
+		"eE",
+		"aA",
+		"",
+		"",
+		"tT"
+	};
 	for (i = 0; i < strLength; i++)
 	{
-		switch (str[i])
-		{
-			case 'a':
-				str[i] = '4';
+		char replace = str[i];
+		int j;
+
+		for (j = 0; j < 8; j++)
+			if (strchr(leetTable[j], replace))
+			{
+				replace = '0' + j;
 				break;
-			case 'A':
-				str[i] = '4';
-				break;
-			case 'e':
-				str[i] = '3';
-				break;
-			case 'E':
-				str[i] = '3';
-				break;
-			case 'o':
-				str[i] = '0';
-				break;
-			case 'O':
-				str[i] = '0';
-				break;
-			case 't':
-				str[i] = '7';
-				break;
-			case 'T':
-				str[i] = '7';
-				break;
-			case 'l':
-				str[i] = '1';
-				break;
-			case 'L':
-				str[i] = '1';
-				break;
-		}
+			}
+
+		str[i] = replace;
 	}
 	return (str);
 }
