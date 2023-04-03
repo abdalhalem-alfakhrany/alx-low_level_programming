83d373ff2864e66e0bb5193bd99b102d3d3e729c
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-03 22:41:02 +0200

    memset Task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _memset - set the n memory with b char
+ * @s: memory pointer
+ * @b: value to be filed with
+ * @n: size to be filled
+ * Return: pointer to memory
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
