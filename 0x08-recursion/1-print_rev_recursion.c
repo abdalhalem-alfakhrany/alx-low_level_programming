3bfa2c085e9512bec810f0ca1d65de16c4271154
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-06 01:59:31 +0200

    print string recursivly reversed Task

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print string recursevly reversed
+ * @s: string to be printed reversly
+ * Retrun: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(++s);
+
+	_putchar(*s);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,5 +3,6 @@
 
 void _putchar(int);
 void _puts_recursion(char *);
+void _print_rev_recursion(char *);
 
 #endif
