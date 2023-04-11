c7ae5a387a760d23c35a5eef912ce354358522a1
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-11 03:10:15 +0200

    multibly the arguments Task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always (0) Succses
+ */
+int main(int argc, char **argv)
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (0);
+	}
+
+	long long a, b;
+
+	a = atoll(argv[1]);
+	b = atoll(argv[2]);
+
+	printf("%ld\n", a * b);
+	return (0);
+}
