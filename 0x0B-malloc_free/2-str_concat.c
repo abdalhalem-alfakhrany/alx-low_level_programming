b9d23e211cb176a21657986ab7bd6d7992b84fef
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-11 20:18:36 +0200

    concat strings Task

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "stdlib.h"
+#include "string.h"
+
+/**
+ * str_concat - concatenate tow string to new one
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to the new string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *new_str;
+	int s1_len = strlen(s1), s2_len = strlen(s2);
+
+	new_str = malloc(s1_len + s2_len);
+	memcpy(new_str, s1, s1_len);
+	memcpy(new_str + s1_len, s2, s2_len);
+	return (new_str);
+}
