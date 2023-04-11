945771c69568a6743eea152557e12379ceeff910
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-11 20:44:44 +0200

    array copying Task

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "stdlib.h"
+#include "memory.h"
+
+/**
+ * alloc_grid - allocate 2d array of size width * height
+ * @width: grid width
+ * @height: grid height
+ * Return: NULL if failer pointer to arrat if Success
+ */
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	array = malloc(height * sizeof(int *));
+	if (!array)
+		return (NULL);
+	for (i = 0; i < width; i++)
+	{
+		array[i] = malloc(width * sizeof(int));
+		if (!array[i])
+			return (NULL);
+		memset(array[i], 0, width);
+	}
+	return (array);
+}
