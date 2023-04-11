d72dbf359f3f2767a4f9af96864169c5d41e9980
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-11 20:05:43 +0200

    array copying Task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,12 +14,12 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	
+
 	str_len = strlen(str);
 	new_str = malloc(str_len * sizeof(char));
 	if (!new_str)
 		return (new_str);
 	memcpy(new_str, str, str_len * sizeof(char));
-
+	new_str[str_len - 1] = '\0';
 	return (new_str);
 }
