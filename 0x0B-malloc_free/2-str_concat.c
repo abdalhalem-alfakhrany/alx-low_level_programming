f219af25c8d0c656584afd7e87f63fc4f66ef13a
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-11 20:21:06 +0200

    concat strings Task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,14 @@ char *str_concat(char *s1, char *s2)
 	char *new_str;
 	int s1_len = strlen(s1), s2_len = strlen(s2);
 
-	new_str = malloc(s1_len + s2_len);
+	new_str = malloc(s1_len + s2_len + 1);
+	if (!new_str)
+		return (NULL);
+
 	memcpy(new_str, s1, s1_len);
 	memcpy(new_str + s1_len, s2, s2_len);
+
+	new_str[s1_len + s2_len] = '\0';
+
 	return (new_str);
 }
