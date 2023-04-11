5e7fb3ff88f0fa34923e9282370f567ee79f10e0
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-11 19:42:58 +0200

    array initialzation Task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,13 +2,19 @@
 #include "stdlib.h"
 #include "memory.h"
 
+/**
+ * create_array - create array and return pointer to it
+ * @size: the size of array
+ * @c: inital char of array
+ * Return: pointer to array
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *array = NULL;
 
-	if(size < 0)
-		return NULL;
-	
+	if (size < 0)
+		return (NULL);
+
 	array = malloc(sizeof(char) * size);
 	memset(array, c, sizeof(char) * size);
 	return (array);
