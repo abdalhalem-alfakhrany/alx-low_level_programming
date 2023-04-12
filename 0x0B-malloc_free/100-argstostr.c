3a695e422d66a855719ab35250f76cb9a6b16e05
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-12 21:50:09 +0200

    concat argv Task

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include "stdlib.h"
+#include "string.h"
+#include "stdio.h"
+
+/**
+ * argstostr - concatinate all argv in one string
+ * @ac: arguments count
+ * @av: arguments vector
+ * Return: NULL if failer pointer to the str if Success
+ */
+char *argstostr(int ac, char **av)
+{
+	char *new_str;
+	int new_str_len, i, arg_len, offset = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 1; i < ac; i++)
+		new_str_len += strlen(av[i]) + 1;
+
+	new_str = malloc(new_str_len);
+
+	if (!new_str)
+		return (NULL);
+
+	for (i = 1; i < ac; i++)
+	{
+		arg_len = strlen(av[i]);
+		memcpy(new_str + offset, av[i], arg_len);
+		offset += arg_len;
+		new_str[offset++] = '\n';
+	}
+
+	return (new_str);
+}
