29f2f65d5a7b1f0864287fb80c8e4b093cf42f74
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-12 04:49:27 +0200

    gridy ready Task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,10 @@ int **alloc_grid(int width, int height)
 
 	array = malloc(height * sizeof(int *));
 	if (!array)
+	{
+		free(array);
 		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		array[i] = malloc(width * sizeof(int));
