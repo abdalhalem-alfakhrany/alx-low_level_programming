f6506476bc0de069433bf3a3c8b6e5e3132f5caa
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-13 12:17:58 +0200

    nconcat Task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,10 +14,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *new_str;
 	int s1_len, s2_len;
 
-	if (s1)
-		s1_len = strlen(s1);
-	if (s2)
-		s2_len = strlen(s2);
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
 
 	n = (n >= s2_len) ? s2_len : n;
 	new_str = malloc(s2_len + s1_len + 1);
