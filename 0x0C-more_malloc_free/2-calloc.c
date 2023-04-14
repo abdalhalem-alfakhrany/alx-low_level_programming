9b7b6361c3401decc35035222e118c9e32e19028
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-14 03:13:12 +0200

    calloc Task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,6 +11,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *array;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
 	array = malloc(nmemb * size);
 	if (!array)
 		return (NULL);
