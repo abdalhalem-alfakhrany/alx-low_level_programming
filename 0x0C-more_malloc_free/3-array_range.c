5bbb7ebeb7d1016f94f76992a71f53acb7d9be4e
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-14 03:41:38 +0200

    array range Task

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,22 +2,22 @@
 #include "stdlib.h"
 
 /**
- * array_range - allocate array of integers sized max - min 
+ * array_range - allocate array of integers sized max - min
  * @min: first number
  * @max: last number
- * Retrun: NULL if failer pointer to arrat if Success
+ * Return: NULL if failer pointer to arrat if Success
  */
 int *array_range(int min, int max)
 {
 	int *array;
 	int length = max - min, i;
-	
+
 	if (length < 0)
 		return (NULL);
 
-	array = malloc(length * sizeof(int) + 1);
-	
-	if(!array)
+	array = malloc(length * sizeof(int) + sizeof(int));
+
+	if (!array)
 		return (NULL);
 
 	for (i = 0; i <= length; i++)
