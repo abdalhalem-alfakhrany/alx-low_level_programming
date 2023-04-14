63d426b20fb74679494baa1790c04022e048e7fe
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-14 02:57:29 +0200

    calloc Task

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * _calloc - alocate nmemb count of size in memory
+ * @nmemb: elements count
+ * @size: size of element
+ * Return: NULL if failer pointer to array if Success
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *array;
+
+	array = malloc(nmemb * size);
+	if (!array)
+		return (NULL);
+
+	return (array);
+}
