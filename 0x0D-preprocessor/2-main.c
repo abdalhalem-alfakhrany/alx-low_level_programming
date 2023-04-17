ddc7535787157d7fcb4eff6f7610f641f09d4238
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-17 16:35:04 +0200

    file compiled name Task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point
+ * Return: always 0 Success
+ */
+int main(void)
+{
+	const char *filename = __FILE__;
+
+	printf("%s\n", filename);
+	return (0);
+}
