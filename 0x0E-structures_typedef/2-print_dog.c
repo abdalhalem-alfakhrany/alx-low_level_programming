5f476158ebb975f97f248a6dcb7473a11d895626
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-17 18:00:22 +0200

    print popy Task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,20 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
+#include "stdio.h"
 
 /**
- * print_dog - prints a struct dog
- * @d: struct dog to print
+ * print_dog - prints dog data
+ * @d: pointer to dog struct
+ * Return: void
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (!d)
 		return;
 
-	if (d->name == NULL)
-		d->name = "(nil)";
-	if (d->owner == NULL)
-		d->owner = "(nil)";
-
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	d->name = (d->name == NULL) ? "(nil)" : d->name;
+	d->owner = (d->owner == NULL) ? "(nil)" : d->owner;
+		
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, (d->age) ? d->age : 0, d->owner);
 }
