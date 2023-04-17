0bc2318bd4e1c2b8c6a4b1396661b3ff63b92c60
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-17 19:01:04 +0200

    new popy Task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -49,16 +49,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name == NULL || age == 0 || owner == NULL)
 		return (NULL);
 
-	dog->name = malloc(_strlen(name));
+	dog->name = malloc(_strlen(name) + 1);
 
 	if (dog->name == NULL)
 	{
 		free(dog->name);
 		free(dog);
-		return (NULL)
+		return (NULL);
 	}
 
-	dog->owner = malloc(_strlen(owner));
+	dog->owner = malloc(_strlen(owner) + 1);
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
