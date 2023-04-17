4772bee45f42b0074d56f478e0b7e706a09943ad
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-17 17:29:14 +0200

    print popy Task

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include "stdio.h"
+
+/**
+ * print_dog - prints dog data
+ * @d: pointer to dog struct
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+	d->name = (d->name == NULL) ? "nil" : d->name;
+	d->owner = (d->owner == NULL) ? "nil" : d->owner;
+	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
+}
