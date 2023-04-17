58789b7ee8a096025b4d970cc20ae2ea65ecf944
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-17 18:55:43 +0200

    new popy Task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,7 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void _memcpy(char *dest, char *src, int size)
+int _strlen(char *str)
 
 #endif
