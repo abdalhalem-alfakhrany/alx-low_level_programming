3d1c8af7119b631cca124758d7416fd6efc74f68
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-19 23:03:20 +0200

    call function pointer Task

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,6 @@
 #ifndef _FUNCTION_POINTERS_
 #define _FUNCTION_POINTERS_
 
-void _putchar(char c);
 void print_name(char *name, void (*f)(char*));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
