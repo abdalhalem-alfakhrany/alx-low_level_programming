f1bf022fd072ac906a9bd5602fd532bf5a84e004
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-19 22:55:12 +0200

    call function pointer Task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print name in terminal by calling function f
+ * @name: the name to be printed
+ * @f: the function pointer to proint function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+		f(name);
+}
