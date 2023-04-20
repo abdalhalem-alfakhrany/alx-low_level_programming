7f4afaf0551bee3e9dd61e0eb20ff1361d3d8eb1
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-20 23:56:44 +0200

    print numbers separated Task

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,18 +12,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
 	unsigned int i;
-	
-	if (!separator)
-		return;
 
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (i > 0)
+		if (i > 0 && separator)
 			printf("%s", separator);
+
 		printf("%d", va_arg(list, int));
 	}
+
 	putchar('\n');
+
 	va_end(list);
 }
