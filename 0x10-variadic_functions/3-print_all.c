2607cec240e745af6f01b836b98aae70618af5a7
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-22 01:46:34 +0200

    print itmes separated Task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,40 @@
-#include "variadic_functions.h"
-#include "stdarg.h"
-#include "stdio.h"
-#include "string.h"
+va_list valist;
+	unsigned int i = 0, j, c = 0;
+	char *str;
+	const char t_arg[] = "cifs";
 
-/**
- * print_all - print all lits itmes sperated be sperator
- * @format: the argument type indicator
- * Return: void
- */
-void print_all(const char * const format, ...)
-{
-	va_list list;
-	int i = 0;
-
-	va_start(list, format);
-
-	while (format[i])
+	va_start(valist, format);
+	while (format && format[i])
 	{
-		if (i > 0 && strchr("cifs", format[i]))
-			printf(", ");
-
-		switch (format[i])
-		{
-			case 'f':
-				printf("%f", va_arg(list, double));
-				break;
-			case 'i':
-				printf("%d", va_arg(list, int));
-				break;
-			case 'c':
-				printf("%c", va_arg(list, int));
-				break;
-			case 's':
-			{
-				char *str = va_arg(list, char *);
-
-				if (str)
-				{
-					printf("%s", str);
-					break;
-				}
-				printf("(nil)");
-			}
-				break;
-			default:
-				break;
-		}
-		i++;
+	j = 0;
+	while (t_arg[j])
+	{
+	if (format[i] == t_arg[j] && c)
+	{
+	printf(", ");
+	break;
+	} j++;
 	}
-	va_end(list);
-	putchar('\n');
-}
+	switch (format[i])
+	{
+	case 'c':
+		printf("%c", va_arg(valist, int)), c = 1;
+		break;
+	case 'i':
+		printf("%d", va_arg(valist, int)), c = 1;
+		break;
+	case 'f':
+		printf("%f", va_arg(valist, double)), c = 1;
+		break;
+	case 's':
+		str = va_arg(valist, char *), c = 1;
+		if (!str)
+		{
+		printf("(nil)");
+		break;
+		}
+		printf("%s", str);
+		break;
+		} i++;
+	}
+	printf("\n"), va_end(valist);
