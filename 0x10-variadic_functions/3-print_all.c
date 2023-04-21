4eba148e37153ec0df6228a37328e3c82e5546f9
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-22 01:51:47 +0200

    print itmes separated Task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,14 @@
-va_list valist;
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
+ *
+ * Return: no return.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list valist;
 	unsigned int i = 0, j, c = 0;
 	char *str;
 	const char t_arg[] = "cifs";
@@ -38,3 +48,4 @@ va_list valist;
 		} i++;
 	}
 	printf("\n"), va_end(valist);
+}
