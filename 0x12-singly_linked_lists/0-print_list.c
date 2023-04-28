2f33dbdd990b2a1c813755abb4532730dd8458e3
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-28 03:36:23 +0300

    0-print_list.c file updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,37 +7,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
-	list_t node;
-
-	if (!h)
-		return (-1);
-
-	node = *h;
-	while (1)
+	size_t count = 0;
+	while (h)
 	{
-		int len;
-		char *s;
-
-		if (node.str == NULL)
-		{
-			s = malloc(5);
-			if (s)
-				s = "(nil)";
-
-			len = 0;
-		}
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 		else
-		{
-			len = node.len;
-			s = node.str;
-		}
-		printf("[%d] %s\n", len, s);
-		i++;
-		if (node.next == NULL)
-			break;
-		node = *node.next;
+			printf("[%d] %s\n", h->len, h->str);
+		count++;
+		h = h->next;
 	}
-
-	return (i);
+	
+	return (count);
 }
