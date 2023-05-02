8caef0122b961d5b51fe7c20756c252ddfbc2ed8
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-05-02 09:55:44 +0300

    5-free_listint2.c file created

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - set the list head to NULL to meke it like empty
+ * @head: pointer to the first list node
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	*head = NULL;
+}
