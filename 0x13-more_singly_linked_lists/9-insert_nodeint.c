9f0a50d218868f9aa10af815f09bacec55d82f95
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-05-02 11:26:49 +0300

    9-insert_nodeint.c file created

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,11 +22,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (i == idx - 1)
 		{
 			listint_t *new_node = malloc(sizeof(listint_t));
-
+			/*
+			 * 0 1 2 3 4 5 6
+			 * 1 2 3 4 5 6 7 insert at 4
+			 * 1 2 3 4 n 5 6 7
+			 * c->n = n;
+			 */
 			new_node->n = n;
 			new_node->next = tmp->next;
-			*tmp = *new_node;
-
+			tmp->next = new_node;
 			return (new_node);
 		}
 		i++;
