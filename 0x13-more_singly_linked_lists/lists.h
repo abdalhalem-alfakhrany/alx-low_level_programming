a7a4f897eae93a741300e0e04168d54c036796ab
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-05-02 09:22:42 +0300

    lists.h file created

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,9 +5,8 @@
 #include "stdlib.h"
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * struct listint_s - singly linked list
+ * @n: the number AKA data
  * @next: points to the next node
  *
  * Description: singly linked list node structure
