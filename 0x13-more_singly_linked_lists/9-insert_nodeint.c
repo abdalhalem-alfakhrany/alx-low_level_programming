7c05d600d97f000e142cb5debd625345b2d26f11
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-05-02 11:50:30 +0300

    9-insert_nodeint.c file updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new_node = malloc(sizeof(listint_t));
 			new_node->n = n;
-
+			if (!new_node)
+				return (NULL);
 			new_node->next = tmp->next;
 			tmp->next = new_node;
 			return (new_node);
