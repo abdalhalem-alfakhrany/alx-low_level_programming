9ba9198d07de733ece6c16c4708f2cd83b115663
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-05-05 04:04:50 +0300

    0-binary_to_uint , main.h , README.md files created

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_
+#define _MAIN_
+#include "string.h"
+#include "ctype.h"
+#include "stdio.h"
+
+unsigned int binary_to_uint(const char *);
+int _pow(int base, int exponent);
+
+#endif
