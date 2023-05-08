fd2b614717fe90f641759243fbe80792c8f68086
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-05-08 10:07:20 +0300

    1-create_file.c file created

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,11 +2,15 @@
 #define _MAIN_
 
 #include "stdio.h"
+#include "string.h"
 #include "stdlib.h"
+#include "errno.h"
 #include "fcntl.h"
 #include "unistd.h"
 #include "stddef.h"
 
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *, size_t);
+int create_file(const char *, char *);
+int append_text_to_file(const char *, char *);
 
 #endif
