54bbf5a91bb248eba8601da27dda203b09434e4b
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-05-08 15:55:47 +0300

    2-append_text_to_file.c file updated

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * append_text_to_file - open the file and add text to end of it
+ * @filename: the file to be manibulated
+ * @text_content: the text to be appende
+ * Return: -1 if failer , 1 if Success
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int file_descriptor, flags, mods;
@@ -7,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	
+
 	flags = O_RDWR | O_APPEND;
 	mods = 0;
 
@@ -20,9 +26,9 @@ int append_text_to_file(const char *filename, char *text_content)
 		if (errno == ENOENT)
 			return (-1);
 	}
-	
+
 	writen_bytes = write(file_descriptor, text_content, strlen(text_content));
-	
+
 	if (writen_bytes == -1)
 		return (-1);
 
