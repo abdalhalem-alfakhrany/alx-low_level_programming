a0c83155dd03daa65402ff72d3ed934e0be20a64
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-07-11 00:52:21 +0300

    README file creatred

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include "stddef.h"
+#include "stdlib.h"
+#include "unistd.h"
+#include "fcntl.h"
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
